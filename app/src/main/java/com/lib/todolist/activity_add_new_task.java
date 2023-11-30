4621b59ee64c989029b32469d661f235861ba036
SBassis <sally2432001@gmail.com>
2023-11-30 22:41:27 +0200

    v1

--- app/src/main/java/com/lib/todolist/activity_add_new_task.java
+++ app/src/main/java/com/lib/todolist/activity_add_new_task.java
@@ -3,6 +3,7 @@ package com.lib.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.collection.ArraySet;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -23,13 +25,14 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+import java.util.Calendar;
 public class activity_add_new_task extends AppCompatActivity {
 
     private SharedPreferences prefs;
     private SharedPreferences.Editor editor;
     public static final String DATA = "DATA";
     ArraySet<Task> listTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,13 +43,19 @@ public class activity_add_new_task extends AppCompatActivity {
         EditText TaskDueDate = findViewById(R.id.etTaskDate);
         Button SaveTaskBtn = findViewById(R.id.savebtn);
 
-      //  setupSharedPrefs();
+        TaskDueDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCalender(TaskDueDate);
+            }
+        });
 
         SaveTaskBtn.setOnClickListener(view -> {
 
-                String description = TaskDescription.getText().toString();
-                boolean status = TaskStatus.isChecked();
-                String dateString = TaskDueDate.getText().toString();
+            String description = TaskDescription.getText().toString();
+            boolean status = TaskStatus.isChecked();
+            String dateString = TaskDueDate.getText().toString();
+
             try {
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
                 Date date = sdf.parse(dateString);
@@ -55,20 +64,40 @@ public class activity_add_new_task extends AppCompatActivity {
                     prefs = getSharedPreferences(MainActivity.DATA, Context.MODE_PRIVATE);
                     editor = prefs.edit();
                     Task newTask = new Task(description, status, date);
-                    List<Task> preTasks = loadTasksFromSharedPreferences(); //previous tasks in the shaerdpref
+                    List<Task> preTasks = loadTasksFromSharedPreferences();
                     preTasks.add(newTask);
                     saveTasksToSharedPreferences(preTasks);
                     Toast.makeText(activity_add_new_task.this, "Task added!", Toast.LENGTH_SHORT).show();
-                    finish(); //close and back to main
+                    finish();
                 } else {
-                    Toast.makeText(activity_add_new_task.this, "Enter The Description!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(activity_add_new_task.this, "Don't let the Description empty!", Toast.LENGTH_SHORT).show();
                 }
 
             } catch (ParseException e) {
                 e.printStackTrace();
             }
         });
+    }
 
+    private void showCalender(final EditText editText) {
+        final Calendar calendar = Calendar.getInstance();
+        int y = calendar.get(Calendar.YEAR);
+        int m = calendar.get(Calendar.MONTH);
+        int d = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int y, int m, int d) {
+                        calendar.set(y, m, d);
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                        editText.setText(dateFormat.format(calendar.getTime()));
+                    }
+                },
+                y, m, d);
+
+        datePickerDialog.show();
     }
 
     private List<Task> loadTasksFromSharedPreferences() {
@@ -88,8 +117,6 @@ public class activity_add_new_task extends AppCompatActivity {
         Gson gson = new Gson();
         String tasksJson = gson.toJson(taskList);
         editor.putString(DATA, tasksJson);
-        editor.commit();
+        editor.commit(); // Use apply() for asynchronous saving
     }
-
-
 }
